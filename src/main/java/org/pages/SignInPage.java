fcee37bacc9ba5ee3e6cb9d9c3eaa76296a72a4b
Yaryna Bilan <yabi@rib-software.dk>
2023-07-26 21:54:55 +0300

    test

--- src/main/java/org/pages/SignInPage.java
+++ src/main/java/org/pages/SignInPage.java
@@ -4,14 +4,15 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SignInPage {
-   private SelenideElement loginInput = $(byXpath("//*[@class='account-text']"));
-   private SelenideElement passwordInput = $(byXpath(""));
+   private SelenideElement loginInput = $(By.xpath("//*[@class='account-text']"));
+   private SelenideElement passwordInput = $(By.name("auth_password"));
 
-   public void inputLogin(String text){
+   public void inputLogin(String text) {
 
       public void inputLogin(String text){
          this.loginInput.val(text);
@@ -19,5 +20,6 @@ public class SignInPage {
          public void inputPassword(String text){
             this.passwordInput.val(text);
          }
+      }
    }
 }
